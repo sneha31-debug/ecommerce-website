8fabf526e0e5536fd9f84b02d2cd0d1992e2a706
sneha31-degug <snehachepurwar6@gmail.com>
2025-06-07 19:10:41 +0530

    cc page added

--- project/src/pages/CustomerCare.jsx
+++ project/src/pages/CustomerCare.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const CustomerCare = () => {
   return (
     <div>
-      <h1>CC</h1>
+      <div>
+        <h1>CUSTOMER CARE</h1>
+        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+      </div>
+      <div>
+        <p>Have any questions or concerns? </p>
+        <p>We’re always ready to help!</p>
+      </div>
+      <div>
+        <p>Call us at</p>
+        <p>123-456-7890</p>
+        <p>or send us an email to:</p>
+        <p>info@mysite.com</p>
+      </div>
     </div>
   )
 }
