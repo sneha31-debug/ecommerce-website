4033b5795da5e1c811f3a786160cec547b25d6df
sneha31-degug <snehachepurwar6@gmail.com>
2025-06-09 19:52:26 +0530

    login page added

---
+++ project/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1>Welcome to Preety Gal</h1>
+        </div>
+        <div>
+            <p>jgashfikhjashdk</p>
+        </div>
+      </div>
+      <div>
+        <h2>USER LOGIN</h2>
+        <input/>
+      </div>
+    </div>
+  )
+}
+
+export default Login
