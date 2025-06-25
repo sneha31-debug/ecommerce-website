9d5a722340c28f488639a37647d109c5e4e983a8
sneha31-degug <snehachepurwar6@gmail.com>
2025-06-25 16:56:05 +0530

    update

--- project/src/pages/Sale.jsx
+++ project/src/pages/Sale.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sale = () => {
   return (
     <div>
-      <h1>Sale xhjkaj saDFSGsdfg</h1>
+      <h1>Sale xhjkaj saDFSGsdfg the summer i turned </h1>
     </div>
   )
 }
